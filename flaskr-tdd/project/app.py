e49882ca9a5bc6dbf1b767878980763935ad1771
Chenyan Wang <chenyan.wang@mail.utoronto.ca>
2021-10-31 06:45:07 -0400

    Added show entries functionality

--- flaskr-tdd/project/app.py
+++ flaskr-tdd/project/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template
 
 
 # configuration
@@ -41,9 +41,13 @@ def close_db(error):
     if hasattr(g, "sqlite_db"):
         g.sqlite_db.close()
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    """Searches the database for entries, then displays them."""
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
 
 
 if __name__ == "__main__":
